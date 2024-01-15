1b6c14ad13d7dd811f4261ee4d4928d5ca5ced71
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2024-01-15 13:05:00 +0600

    added gitignore and move csv to data folder

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from salary_generator import generate_csv_data, write_csv, read_csv
 
 if __name__ == '__main__':
-    filename = 'salaries_100.csv'
+    filename = 'data/salaries_100.csv'
 
     # write data
     data = generate_csv_data(100)
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     write_csv(filename, data)
 
     # read data
-    dataFrame = pd.read_csv('salaries_100.csv')
+    dataFrame = pd.read_csv('data/salaries_100.csv')
     print(dataFrame)
