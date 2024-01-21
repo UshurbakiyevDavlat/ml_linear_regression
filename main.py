9e9ee794545c17706d3ddc4238c174c4fc719470
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2024-01-21 16:47:23 +0600

    training and testing data spliting

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.model_selection import train_test_split
 from salary_generator import generate_csv_data, write_csv
 
 def generate_and_write_csv(file):
@@ -17,5 +18,12 @@ if __name__ == '__main__':
     x = df.iloc[:,:-1].values # get all rows with all columns except the last one
     y = df.iloc[:,-1].values # get all rows with only the last column #
 
+    # random_state if unset is None, and it will randomize trained data every time
+    # if it's any integer then it is identification and nothing more.
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+
     plt.scatter(x,y)
     plt.show()
+
+    print(x_train.shape)
+    print(x_test.shape)
