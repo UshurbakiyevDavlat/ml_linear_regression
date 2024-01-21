eff4f992195caf98cf7235e39f2e24876fe81a0c
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2024-01-21 12:35:45 +0600

    updated readme and added dependent and independent variables and use matplotlib scatter to show regression

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Linear regression on python.
+# Linear regression on python.
+
+So, Mathematically, linear regression can be described as a formula:
+
+~~~
+Y = a + b * X
+~~~
+
+The goal of ML model is to build a best-fitting straight line according to that formula.
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 from salary_generator import generate_csv_data, write_csv
 
-def generate_and_write_csv():
-    filename = 'data/salaries_100.csv'
+def generate_and_write_csv(file):
     data = generate_csv_data(100)
-    write_csv(filename, data)
+    write_csv(file, data)
 
 
 if __name__ == '__main__':
-    generate_and_write_csv()
+    file_name = 'data/salaries_100.csv'
+    df = pd.read_csv(file_name)
+
+    # let's find out dependent and independent variable values
+    x = df.iloc[:,:-1].values # get all rows with all columns except the last one
+    y = df.iloc[:,-1].values # get all rows with only the last column #
+
+    plt.scatter(x,y)
+    plt.show()
