62a3ecf483e3fb028a2f1eda3ef5967bf654271c
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2024-01-21 17:18:16 +0600

    linear regression model, training, prediction, error, and checking accuracy

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from salary_generator import generate_csv_data, write_csv
+
 
 def generate_and_write_csv(file):
     data = generate_csv_data(100)
@@ -15,15 +18,29 @@ if __name__ == '__main__':
     df = pd.read_csv(file_name)
 
     # let's find out dependent and independent variable values
-    x = df.iloc[:,:-1].values # get all rows with all columns except the last one
-    y = df.iloc[:,-1].values # get all rows with only the last column #
+    x = df.iloc[:, :-1].values  # get all rows with all columns except the last one, so here the years of expirience
+    y = df.iloc[:, -1].values  # get all rows with only the last column # and here is salaries
 
     # random_state if unset is None, and it will randomize trained data every time
     # if it's any integer then it is identification and nothing more.
-    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+    # 80/20 , 80% - training data, 20% - test data
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-    plt.scatter(x,y)
+    model = LinearRegression()
+    model.fit(x_train, y_train)  # training model on years of exp and salaries relationship, uses 80% data
+    y_pred = model.predict(x_test)  # predicting salaries for 20% testing years of exp
+
+    # as long as out model trained now we can use even additional years of experience
+    salaries = model.predict([[11], [13], [14.8], [
+        50]])  # argument is matrix, because fit() and predict() accept only this kind of data type.
+
+    # diff in accuracy between predicted and testing salaries
+    error = y_pred - y_test
+
+    r2 = r2_score(y_test, y_pred)
+    print(f"R2 score: {r2} ({r2:.2%})")
+
+    plt.scatter(x_test, y_test)
+    plt.plot(x_test, y_pred, color="yellow")
+
     plt.show()
-
-    print(x_train.shape)
-    print(x_test.shape)
